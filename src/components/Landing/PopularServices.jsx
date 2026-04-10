d7f4d6df8d61159e0b9c207804cf0208c4dc7f4d
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-10 13:54:53 +0100

    feat: creation du structure principale de Popular Services section

--- src/components/Landing/PopularServices.jsx
+++ src/components/Landing/PopularServices.jsx
@@ -1,1 +1,34 @@
-export default function PopularServices() {}
+import { FaArrowRightLong } from "react-icons/fa6";
+import { MdPlumbing } from "react-icons/md";
+
+export default function PopularServices() {
+    return (
+        <section>
+            <div>
+                <div>
+                    <h2>Popular Services</h2>
+                    <p>Get your home tasks handled by experts</p>
+                </div>
+                <div>
+                    <a href="#">
+                        View all services
+                        <FaArrowRightLong />
+                    </a>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <MdPlumbing />
+                    </div>
+                    <h4>
+                        Plumbing
+                    </h4>
+                    <p>
+                        Leaky pipes & taps
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
