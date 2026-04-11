3fc71f7977ae7420d7995cacd340b86c56f50909
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-11 11:57:34 +0100

    feat: transforme card dans PopularServices a une components seul

--- src/components/Landing/PopularServices.jsx
+++ src/components/Landing/PopularServices.jsx
@@ -1,5 +1,5 @@
 import { FaArrowRightLong } from "react-icons/fa6";
-import { MdPlumbing } from "react-icons/md";
+import Cards from "./PopularServices/Cards";
 
 export default function PopularServices() {
     return (
@@ -26,21 +26,7 @@ export default function PopularServices() {
                 </a>
             </div>
             <div className="grid grid-cols-6 gap-8">
-                <div className="p-6 bg-white border-2 border-[#F1F5F9] rounded-2xl w-52 flex flex-col gap-5">
-                    <div className="w-16 h-16 bg-[rgba(249,116,21,0.15)] rounded-full flex items-center justify-center">
-                        <MdPlumbing className="text-[#F97415]" size={28} />
-                    </div>
-
-                    <div className="space-y-1">
-                        <h4 className="text-[#0F172A] font-semibold text-lg">
-                            Plumbing
-                        </h4>
-
-                        <p className="text-[#94A3B8] text-sm">
-                            Leaky pipes & taps
-                        </p>
-                    </div>
-                </div>
+                <Cards />
             </div>
         </section>
     )
---
+++ src/components/Landing/PopularServices/Cards.jsx
@@ -0,0 +1,21 @@
+import { MdPlumbing } from "react-icons/md";
+
+export default function Cards() {
+    return (
+        <div className="p-6 bg-white border-2 border-[#F1F5F9] rounded-2xl w-52 flex flex-col gap-5">
+            <div className="w-16 h-16 bg-[rgba(249,116,21,0.15)] rounded-full flex items-center justify-center">
+                <MdPlumbing className="text-[#F97415]" size={28} />
+            </div>
+
+            <div className="space-y-1">
+                <h4 className="text-[#0F172A] font-semibold text-lg">
+                    Plumbing
+                </h4>
+
+                <p className="text-[#94A3B8] text-sm">
+                    Leaky pipes & taps
+                </p>
+            </div>
+        </div>
+    )
+}
