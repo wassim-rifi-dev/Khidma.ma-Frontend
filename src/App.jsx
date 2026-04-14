4b4c1159edd5aa42d994a7aa80c1bfa21354e3e8
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-14 16:59:16 +0100

    feat: ajoute AppRoutes ou il y a routing systeme

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@
 
 import Register from "./pages/Auth/Register";
 import AuthProvider from "./context/AuthProvider";
+import AppRoutes from "./routes/AppRoutes";
+import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
     // const {dark , changeMode} = useDarkMode();
     return (
         // <Landing isDark={dark} toogleDark={changeMode} />
         <AuthProvider>
-            <Register />
+            <BrowserRouter>
+                <AppRoutes />
+            </BrowserRouter>
         </AuthProvider>
     )
 }
