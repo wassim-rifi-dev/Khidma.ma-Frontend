7d1020b134d9b82927f4d586be427e6683d2ee96
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-14 15:08:15 +0100

    feat: ajoute request interactor pour include le token  automatique dans l'API request

--- src/services/api.js
+++ src/services/api.js
@@ -5,4 +5,14 @@ export const api = axios.create({
     headers: {
         "Content-Type": "application/json",
     },
+});
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config;
 })
