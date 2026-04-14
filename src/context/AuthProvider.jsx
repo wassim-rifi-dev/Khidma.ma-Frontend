6fd24214d150998d4248eb35466342f345c60c5a
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-14 15:27:35 +0100

    feat: ajoute AuthProvider et le logique de register

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import * as authServices from '../services/authServices';
+import { AuthContext } from "./AuthContext";
+
+export default function AuthProvider({children}) {
+    const [user , setUser] = useState(null);
+
+    async function register(data) {
+        try {
+            const res = await authServices.registerUser(data);
+
+            const {user , token} = res.data;
+
+            localStorage.setItem("token" , token);
+            
+            setUser(user);
+
+            return res;
+        } catch (error) {
+            throw error.response?.data || error.message;
+        }
+    }
+
+    return (
+        <AuthContext.Provider value={{ user , register }} >
+            { children }
+        </AuthContext.Provider>
+    )
+
+}
