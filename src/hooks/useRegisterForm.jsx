db13995a0ce4b9e2781a4c8c345aa9f3bf5ab461
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-14 15:49:50 +0100

    feat: creation du useRegisterForm hooks

---
+++ src/hooks/useRegisterForm.jsx
@@ -0,0 +1,33 @@
+import { useContext, useState } from "react"
+import { AuthContext } from "../context/AuthContext";
+
+export function useRegisterForm() {
+    const {register} = useContext(AuthContext);
+
+    const [form , setForm] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        role: '',
+        password: '',
+        password_confirmation: '',
+        category: '',
+        city: ''
+    });
+
+    function handleChange(e) {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        try {
+            await register(form);
+        } catch (err) {
+            console.error("Error : " , err);
+        }
+    }
+
+    return {form , handleChange , handleSubmit}
+}
