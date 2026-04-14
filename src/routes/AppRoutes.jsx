e97c2db0ef488bbe8516029962b79c50f126f70a
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-14 17:00:32 +0100

    feat: ajoute route du page de register

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,10 +1,12 @@
 import {Route, Routes} from 'react-router-dom';
 import Landing from '../pages/Landing';
+import Register from '../pages/Auth/Register';
 
 export default function AppRoutes() {
     return (
         <Routes>
             <Route path='/' element={<Landing />} />
+            <Route path='/register' element={<Register />} />
         </Routes>
     )
 }
