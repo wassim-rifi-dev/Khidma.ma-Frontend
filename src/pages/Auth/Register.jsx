79f19331bd73d18d9278860cdb4bc5e4b74d8541
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-15 10:16:30 +0100

    feat: creer layout de footer pour auth

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,9 +1,11 @@
+import Footer from "../../components/Auth/Layouts/Footer"
 import Header from "../../components/Auth/Layouts/Header"
 
 export default function Register() {
     return (
         <div className="flex flex-col">
             <Header />
+            <Footer />
         </div>
     )
 }
