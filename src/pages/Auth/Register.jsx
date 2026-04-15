ccbcda709b5827b5e8011ea954104f88c9943081
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-15 10:28:07 +0100

    feat: creer Register form

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,10 +1,14 @@
 import Footer from "../../components/Auth/Layouts/Footer"
 import Header from "../../components/Auth/Layouts/Header"
+import StepRole from "../../components/Auth/Register/Steps/StepRole"
 
 export default function Register() {
     return (
         <div className="flex flex-col">
             <Header />
+            <main className="flex flex-col gap-5">
+                <StepRole />
+            </main>
             <Footer />
         </div>
     )
