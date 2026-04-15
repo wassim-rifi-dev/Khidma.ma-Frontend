e115803f025e033d726652fe58deacca4f4abff4
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-15 10:29:41 +0100

    feat: ajoute structure principale de register form

---
+++ src/components/Auth/Register/RigisterForm.jsx
@@ -0,0 +1,9 @@
+import StepRole from "./Steps/StepRole";
+
+export default function RegisterForm() {
+    return (
+        <form>
+            <StepRole />
+        </form>
+    )
+}
--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,13 +1,13 @@
 import Footer from "../../components/Auth/Layouts/Footer"
 import Header from "../../components/Auth/Layouts/Header"
-import StepRole from "../../components/Auth/Register/Steps/StepRole"
+import RegisterForm from "../../components/Auth/Register/RigisterForm"
 
 export default function Register() {
     return (
         <div className="flex flex-col">
             <Header />
             <main className="flex flex-col gap-5">
-                <StepRole />
+                <RegisterForm />
             </main>
             <Footer />
         </div>
