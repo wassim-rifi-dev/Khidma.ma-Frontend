652b21abd9286d98df429514af7e51bf8d4a8549
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-15 15:54:05 +0100

    feat: ajoute structure principale de page de login

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,3 +1,16 @@
+import Footer from "../../components/Auth/Layouts/Footer";
+import Header from "../../components/Auth/Layouts/Header";
+
 export default function Login() {
-    return ()
+    return (
+        <div className="flex flex-col min-h-screen bg-[#F8F9FA]">
+            <Header />
+
+            <main className="flex flex-col items-center justify-center flex-1 gap-5 py-10">
+                
+            </main>
+
+            <Footer />
+        </div>
+    )
 }
