6421d8731d98d0b0762b941542eee6c671a469eb
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-15 11:18:45 +0100

    feat: ajoute design au structure principale de la page de register

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -4,11 +4,13 @@ import RegisterForm from "../../components/Auth/Register/RigisterForm"
 
 export default function Register() {
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col min-h-screen bg-[#F8F9FA]">
             <Header />
-            <main className="flex flex-col gap-5">
+
+            <main className="flex flex-col items-center justify-center flex-1 gap-5 py-10">
                 <RegisterForm />
             </main>
+
             <Footer />
         </div>
     )
