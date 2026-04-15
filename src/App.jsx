af13fddfaedd6170f1956dc86d88f26023df8afa
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-15 17:06:42 +0100

    fix: dark mode n'existe pas comme function

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-// import { useDarkMode } from "./hooks/useDarkMode";
-// import Landing from "./pages/Landing";
-
 import Register from "./pages/Auth/Register";
 import AuthProvider from "./context/AuthProvider";
 import AppRoutes from "./routes/AppRoutes";
 import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
-    // const {dark , changeMode} = useDarkMode();
     return (
-        // <Landing isDark={dark} toogleDark={changeMode} />
         <AuthProvider>
             <BrowserRouter>
                 <AppRoutes />
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import {Route, Routes} from 'react-router-dom';
+import { useDarkMode } from '../hooks/useDarkMode';
 import Landing from '../pages/Landing';
 import Register from '../pages/Auth/Register';
 import Login from '../pages/Auth/Login';
 
 export default function AppRoutes() {
+    const {dark , changeMode} = useDarkMode();
     return (
         <Routes>
-            <Route path='/' element={<Landing />} />
+            <Route path='/' element={<Landing isDark={dark} toogleDark={changeMode} />} />
             <Route path='/register' element={<Register />} />
             <Route path='/login' element={<Login />} />
         </Routes>
