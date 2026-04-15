312b7328fefdcb3b8de450fe9bfc73ca51ced8cc
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-15 15:52:56 +0100

    feat: ajoute login route

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import {Route, Routes} from 'react-router-dom';
 import Landing from '../pages/Landing';
 import Register from '../pages/Auth/Register';
+import Login from '../pages/Auth/Login';
 
 export default function AppRoutes() {
     return (
         <Routes>
             <Route path='/' element={<Landing />} />
             <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
         </Routes>
     )
 }
