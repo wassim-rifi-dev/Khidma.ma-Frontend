cb7567ed2a5e4b20efb9ba72f523347f314de2ee
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-20 16:06:59 +0100

    feat: ajoute consomation d'API de creation d'une message

--- src/services/ChatServices.js
+++ src/services/ChatServices.js
@@ -2,7 +2,16 @@ import { api } from "./api";
 
 export async function getChatMessages(chatId) {
     try {
-        const response = await api.get(`chat/${chatId}`);
+        const response = await api.get(`/chat/${chatId}`);
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
+}
+
+export async function createMessage(chatId , data) {
+    try {
+        const response = await api.post(`/message/store/${chatId}` , data);
         return response.data;
     } catch (error) {
         throw error.response?.data || error.message;
