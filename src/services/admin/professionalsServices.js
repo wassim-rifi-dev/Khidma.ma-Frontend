0c49b19ec9f4adcd142906d6aa8cb2c417602fc8
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-23 16:22:44 +0100

    feat: ajoute  consomation de updateVerifyProfessionalStatus Api

--- src/services/admin/professionalsServices.js
+++ src/services/admin/professionalsServices.js
@@ -9,3 +9,13 @@ export async function getAllProfessional() {
         throw error.response?.data || error.message;
     }
 }
+
+export async function updateVerifyProfessionalStatus(professionalId , data) {
+    try {
+        const response = await api.patch(`admin/professionals/${professionalId}/verify` , data);
+
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
+}
