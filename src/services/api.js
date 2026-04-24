3dbc5a6eb0c1cf9daaee69dc3d6ca9ffa374e213
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-04-24 16:27:40 +0100

    feat: update API base URL to production server

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'http://127.0.0.1:8000/api';
+export const API_BASE_URL = 'http://51.20.35.235/api';
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
